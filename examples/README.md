734dcdc6bbfb9b869384726242ade9419b37ccbd
kewalaka <kewalaka@gmail.com>
2023-12-03 10:51:51 +1300

    feat: add database example

--- examples/README.md
+++ examples/README.md
@@ -1,5 +1,16 @@
 # Examples
 
+This module is for the Azure SQL database.  Managed instances & SQL VMs will be created as separate modules.  The following examples illustrate the use of this module:
+
+- [database](./database/) - an mssql server and a single database.
+- [default](./default/) - this will create a mssql server.
+- [elastic_pool](./elastic_pool/) - this will create a mssql server and one elastic pool.
+- [elastic_pool_database](./elastic_pool_database/) - this augments the elastic pool example to add a database.
+
+## Contributing new examples
+
+New examples to test functionality or illustrate use are welcome.
+
 - Create a directory for each example.
 - Create a `_header.md` file in each directory to describe the example.
 - See the `default` example provided as a skeleton - this must remain, but you can add others.
---
+++ examples/database/main.tf
@@ -0,0 +1,93 @@
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0, < 4.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "enable_telemetry" {
+  type        = bool
+  default     = true
+  description = <<DESCRIPTION
+This variable controls whether or not telemetry is enabled for the module.
+For more information see<https://aka.ms/avm/telemetryinfo>.
+If it is set to false, then no telemetry will be collected.
+DESCRIPTION
+}
+
+# This ensures we have unique CAF compliant names for our resources.
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  version = "0.3.0"
+}
+
+# This is required for resource modules
+resource "azurerm_resource_group" "this" {
+  name     = module.naming.resource_group.name_unique
+  location = "AustraliaEast"
+}
+
+resource "random_password" "admin_password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+locals {
+  tags = {
+    environment = "sample"
+    cost_centre = "demo"
+  }
+
+  databases = {
+    my_sample_db = {
+      create_mode  = "Default"
+      collation    = "SQL_Latin1_General_CP1_CI_AS"
+      server_id    = module.sql_server.resource.id
+      license_type = "LicenseIncluded"
+      max_size_gb  = 50
+      sku_name     = "S0"
+
+      short_term_retention_policy = {
+        retention_days           = 1
+        backup_interval_in_hours = 24
+      }
+
+      long_term_retention_policy = {
+        weekly_retention  = 2
+        monthly_retention = 2
+        yearly_retention  = 1
+        week_of_year      = 1
+      }
+
+      tags = local.tags
+    }
+  }
+}
+
+# This is the module call
+module "sql_server" {
+  source = "../../"
+  # source             = "Azure/avm-<res/ptn>-<name>/azurerm"
+  # ...
+  enable_telemetry             = var.enable_telemetry
+  name                         = module.naming.sql_server.name_unique
+  resource_group_name          = azurerm_resource_group.this.name
+  administrator_login          = "mysqladmin"
+  administrator_login_password = random_password.admin_password.result
+
+  databases = local.databases
+
+  tags = local.tags
+}
